84b0921ccabfd57a0f8024c2837442d14d97e883
Anthony Ellis <amellis245@gmail.com>
2021-11-21 12:26:26 -0500

    created new employee test to test data being passed throughthe constructor parameters

--- test/employee.test.js
+++ test/employee.test.js
@@ -1,9 +1,11 @@
 const Employee = require("../lib/employee");
 
 describe("Employee Test", () => {
-    test("Should return objects", () => {
+    test("Should return new Employee object", () => {
         const newemp = new Employee();
 
         expect(typeof newemp).toBe("object");
     });
+
+    test("Should be able to pass parameters through constructor", () => {});
 });
