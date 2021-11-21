71da80f238f6602597bdd4f790fcb87a475bfc71
Anthony Ellis <amellis245@gmail.com>
2021-11-21 14:20:22 -0500

    created main prompt

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+const promptobj = [
+    {
+        // What is your name?
+        type: "input",
+        message: "What is a your name",
+        name: "name",
+    },
+
+    {
+        // What role do you have?
+        type: "list",
+        message: "What position do you work",
+        choices: ["Manager", "Engineer", "Employee"],
+        name: "role",
+    },
+
+    //
+];
