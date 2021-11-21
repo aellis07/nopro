6cc7220126e203d24455e8bdbaeae959a50ab580
Anthony Ellis <amellis245@gmail.com>
2021-11-21 12:36:21 -0500

    Passed the test of passing data into the Engineering sub-Employee constructor

--- test/engineer.test.js
+++ test/engineer.test.js
@@ -1,5 +1,10 @@
 const Engineer = require("../lib/engineer");
 
 describe("Engineer Test", () => {
-    test("Should be able to pass argument through subclass", () => {});
+    test("Should be able to pass argument through subclass", () => {
+        const githubb = "aellis07";
+        const newemp = new Engineer("Anthony", 2, "fake@mail.com", githubb);
+
+        expect(newemp.github).toBe(githubb);
+    });
 });
