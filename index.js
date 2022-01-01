eeb93043dfbc751edaf34817e3c958282d463523
Anthony Ellis <amellis245@gmail.com>
2022-01-01 11:16:16 -0500

    created code literals for each type of employee

--- index.js
+++ index.js
@@ -143,19 +143,87 @@ function mainPrompt() {
   });
 }
 
+// Creating a code literal and passing the respective employee object into the function
+const generateManager = function (manager) {
+  return ` <div class="card">
+        <div class="card-header" id="cardHeader">
+          <h5 class="name">${manager.name}</h5>
+          <h6 class="card-title">Manager</h6>
+        </div>
+
+        <div class="card-body">
+          <p class="cardId">Employee ID: ${manager.empID}</p>
+          <address>
+            <p class="cardEmail">
+              Email: ${manager.email}
+              <a href="mailto:${manager.email}"></a>
+            </p>
+          </address>
+          <p class="cardNumber">Office Number: ${manager.number}</p>
+        </div>
+        </div>`;
+};
+
+const generateEngineer = function (engineer) {
+  return ` <div class="card">
+          <div class="card-header" id="cardHeader">
+            <h5 class="name">${engineer.name}</h5>
+            <h6 class="card-title">Engineer</h6>
+          </div>
+  
+          <div class="card-body">
+            <p class="cardId">Employee ID: ${engineer.empID}</p>
+            <address>
+              <p class="cardEmail">
+                Email: ${engineer.email}
+                <a href="mailto:${engineer.email}">  </a>
+              </p>
+            </address>
+            <p class="cardGithub">Github: ${engineer.github}</p>
+          </div>
+          </div>`;
+};
+
+const generateIntern = function (intern) {
+  return ` <div class="card">
+          <div class="card-header" id="cardHeader">
+            <h5 class="name">${intern.name}</h5>
+            <h6 class="card-title">Intern</h6>
+          </div>
+  
+          <div class="card-body">
+            <p class="cardId">Employee ID: ${intern.empID}</p>
+            <address>
+              <p class="cardEmail">
+                Email: ${intern.email}
+                <a href="mailto:${intern.email}"> </a>
+              </p>
+            </address>
+            <p class="cardSchool">School: ${intern.school}</p>
+          </div>
+          </div>`;
+};
+
 //
 function generate(output, teamMembers) {
+  newTeamArr = [];
+  // Looping through teamMembers array
   for (let i = 0; i < teamMembers.length; i++) {
     const employee = teamMembers[i];
     const role = employee.getRole();
 
+    // Sorting employess by role using the getRole method we created in the employee class
     if (role === "Manager") {
+      // Taking manager info and pushing it into the generateManager code literal
+      newTeamArr.push(generateManager(employee));
       console.log("Managers: ", employee);
     }
     if (role === "Engineer") {
+      newTeamArr.push(generateEngineer(employee));
       console.log("Engineers: ", employee);
     }
     if (role === "Intern") {
+      newTeamArr.push(generateIntern(employee));
       console.log("Intern: ", employee);
     }
   }
