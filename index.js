f2098555f665d4377ec41a54d9a7bccb6d670763
Anthony Ellis <amellis245@gmail.com>
2022-01-01 10:41:08 -0500

    updated prompt and imported employee classes

--- index.js
+++ index.js
@@ -1,73 +1,34 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 
+const teamMembers = [];
 const promptobj = [
-    {
-        // What is your name?
-        type: "input",
-        message: "What is a your name",
-        name: "name",
-    },
-
-    {
-        // What role do you have?
-        type: "list",
-        message: "What position do you work",
-        choices: ["Manager", "Engineer", "Employee"],
-        name: "role",
-    },
-
-    //
+  {
+    // What is your name?
+    type: "input",
+    message: "What is a your name",
+    name: "name",
+  },
+  {
+    // What is your employee id?
+    type: "input",
+    message: "What is a your employee id?",
+    name: "id",
+  },
+  {
+    // What is your email?
+    type: "input",
+    message: "What is a your email?",
+    name: "email",
+  },
+  {
+    // What is your role?
+    type: "list",
+    message: "What is  your role",
+    choices: ["Manager", "Engineer", "Intern"],
+    name: "role",
+  },
 ];
-
-inquirer.prompt(promptobj).then((response) => {
-    if (response.role == "Manager") {
-        console.log("Manager role was selected");
-
-        const managerresponse = [
-            {
-                type: "number",
-                message: "What is a your office number?",
-                name: "number",
-            },
-        ];
-
-        inquirer.prompt(managerresponse).then(() => {
-            const newEmployeePrompt = [
-                {
-                    type: "confirm",
-                    message: "Would you like to add a new employee",
-                    name: "newEmployee",
-                },
-            ];
-        });
-    }
-    if (response.role == "Engineer") {
-        console.log("Engineer role was selected");
-
-        const engineerresponse = [
-            {
-                type: "input",
-                message: "What is a your github?",
-                name: "github",
-            },
-        ];
-
-        inquirer.prompt(engineerresponse);
-    }
-    if (response.role == "Intern") {
-        console.log("Intern role was selected");
-
-        const internresponse = [
-            {
-                type: "input",
-                message: "What school did you go to?",
-                name: "number",
-            },
-        ];
-
-        inquirer.prompt(internresponse);
-    }
-
-    // console.log(response);
-});
